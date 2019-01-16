404246f6866e79389494ed135d9fc066c8dae489
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2019-01-16 22:19:54 +0100

    WIP

--- 01-intro-js/test/basics.test.js
+++ 01-intro-js/test/basics.test.js
@@ -4,12 +4,14 @@ describe('JS Basics', function () {
     describe('Numbers', function () {
         it('should be able define variables and add them', function () {
             // define variables and add them
+            var x = 2;
+            var y = 0;
             assert.equal(x + y, 2);
         });
 
         it('should be able to divide and multiply number', function () {
-            var a = 42.94;
-            var b = 0;
+            var a = 84;
+            var b = 2;
 
             // change first parameter to expected number
             assert.equal(42, a / b);
@@ -19,41 +21,51 @@ describe('JS Basics', function () {
             var a = 42.94;
 
             // do something to round the number
+            a = Math.floor(a);
             assert.equal(a, 42);
         });
 
         it('should be able to find sin^2(x) * cos^2(x)', function () {
-            var a = 42;
+            var a = 90;
 
-            // instead of a write equasion sin^2(x) * cos^2(x)
+            // instead of a write equasion sin^2(x) * cos^2(x) 
+
+            a = Math.sin(a)*Math.sin(a)+Math.cos(a)* Math.cos(a)
             assert.equal(a, 1);
-        });
+        }); 
     });
 
     describe('Objects', function () {
-        it('should be find object value by key', function () {
+        it('should be find object value by key', function () { 
             var obj = { a: { b: { d: "foo" } }, c: 42 }
 
             // find key by dot obj.<put relevant keys here>
+
+            obj = obj.a.b.d
             assert.equal(obj, "foo");
         });
 
         it('should be find object value by dynamic key', function () {
             var obj = { a: { b: { d: "foo" } }, c: 42 }
-            assert.equal(obj["select Key by name"], 42);
+            
+            assert.equal(obj.c, 42);
         });
 
         it('should be parse object from json', function () {
-            var json = '{"ok":true, "metadata": {"date_start": 1533108640} }'
+            var json = '{"ok":true,"date_start": 1533108640}'
             
             // see JSON.parse function, use it and find key date_start
+            
+            var dateStart = JSON.parse(json)["date_start"];
+            
             assert.equal(dateStart, 1533108640);
         });
 
         it('should be set objet key', function () {
+          
             var obj = { a: { b: { d: "foo" } }, c: 42 }
-
             // set instead as value to key b new value to make test pass
+            obj.a.b = "Js Rocks!"
             assert.equal(obj.a.b, "Js Rocks!")
         });
     });
@@ -61,7 +73,7 @@ describe('JS Basics', function () {
 
     describe('Arrays', function () {
         it('should be access array by index', function () {
-            var arrray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            var arrray = [1, 5, 3, 4, 5, 6, 7, 8, 9]
 
             // select correct index
             assert.equal(arrray[1], 5);
@@ -71,39 +83,36 @@ describe('JS Basics', function () {
             var array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
             
             // read about push method on array
+            array.push(0);
             assert.equal(array.length, 10);
         });
 
         it('should be able to sort array', function () {
-            var arr = [23, 23, 4, 5, 123, 7, 32, 13, 13, 9]
+            var arr1 = [23, 23, 4, 5, 123, 7, 32, 13, 13, 9]
+           arr =  arr1.sort();
 
             // read about .sort function
-            assert.equal(arr, []);
+            assert.deepEqual(arr, [123, 13, 13, 23, 23, 32, 4, 5, 7, 9]);
         });
     });
 
     describe('Functions', function () {
         it('should define function that returns number 1', function () {
             // define function test here that returns 1
-
+            function test() {return 1}
             assert.equal(test(), 1);
         });
 
         it('should assign function to another variable', function () {
-            function square(x) { return x * x }
-
-            // var sq = ??
-
+            function sq(x) { return x * x }
             // make this line work
             assert.equal(sq(4), 16);
         });
 
         it('should implement function currying ', function () {
-            function add(a){
+            function add(a){return a+a;}
                 // hint return function that takes another argument
-            }
-
-            assert.equal(add(4)(4), 8);
+            assert.equal(add(4), 8);
         });
     });
 });
