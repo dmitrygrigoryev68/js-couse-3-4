f5cf0e7039fb390647145ce5112db174c0fac7b1
dmitrygrigoryev68 <dmitry.grigoryev@gmx.com>
2019-01-29 18:47:33 +0100

    Progress dz-4-6

--- 05-basic-js-animation/app.js
+++ 05-basic-js-animation/app.js
@@ -1,21 +1,32 @@
-(function(){
-    var div = document.querySelector('div');
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 20;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     
-    var initTop = 85;
-    var initLeft = 0;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
 
-    var step = 1;
-
-    var intervalId = setInterval(function(){
-        if (initTop >= 0 && initLeft < 100){
-            initTop = initTop - step;
-            initLeft = initLeft + step;
-        } else {
-            clearInterval(intervalId);
-        }
-
-        div.style.top = initTop + "%"
-        div.style.left = initLeft + "%"
-
-    }, 100);
-})()
+setInterval(draw, 10);
